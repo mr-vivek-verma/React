de1e28df478f526e1b9578470805f881c5d9f7ef
Vivek Verma <mr.vivek2090@gmail.com>
2023-12-22 15:33:06 +0530

    top rated feature added
    filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,40 @@
 import RestaurantCard from "./RestaurantCard";
+import { useEffect, useState } from "react";
 import resList from "../utils/mockData";
 
 const Body = () => {
-    return(
-        <div className='body'>
-        <div className='search'>Search</div>
-        <div className='restaurant-container'>
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  useEffect(() => {
+    fetchData();
+  }, [])
+  const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=26.890759&lng=81.059376&carousel=true&third_party_vendor=1");
+    const json = await data.json();
+    console.log(json);
+  };                                        
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants?.filter(      
+              (res) => res.info.avgRating > 4  
+            ); 
+            setListOfRestaurants(filteredList);
+          }} 
+        >                  
+          TopRatedRestaurant
+        </button>
+      </div>
+      <div className="restaurant-container">
         {/* So here we have passed props */}
-        {
-            resList.map(restaurant => <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)
-        }
-        </div>
-        </div>
-    )
-}
+        {listOfRestaurants?.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,17 +1,17 @@
 import {CDN_URL} from "./../utils/constants";
-
 const RestaurantCard = (props) => {
     
     const {resData} = props;
 
-    const {cloudinaryImageId,name,cuisines,avgRating,deliveryTime } = resData?.info;
+    const {cloudinaryImageId,name,cuisines,avgRating,deliveryTime,costForTwo } = resData?.info;
 
     return(
         <div className='restaurant-card'>
         <img className='restaurant-logo' alt='res-logo' src={CDN_URL+ cloudinaryImageId}/>
             <h3>{name}</h3>
             <h4>{cuisines.join(",")}</h4>
-            <h4>{avgRating}</h4>
+            <h4>{avgRating} stars</h4>
+            <h4>{costForTwo}</h4>
             <h4>{deliveryTime}</h4>
         </div>
     )
